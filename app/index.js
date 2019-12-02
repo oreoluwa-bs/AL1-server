bf09683239635b58fca8a7e84f1c6a2feebc4fcb
Oreoluwa Bimbo-Salami <oreoluwabs25@gmail.com>
2019-12-02 04:08:52 +0100

    Added express bodyparser and CORS

--- app/index.js
+++ app/index.js
@@ -2,6 +2,18 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true,
+}));
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
 app.use('/', (req, res) => {
     res.status(200).json({
         status: '200',
@@ -15,3 +27,5 @@ app.listen(port, () => {
     // eslint-disable-next-line no-console
     console.log(`App is running on port ${port}`);
 });
+
+module.exports = app;
